a1a9d45644ac0ae597c1cd5cc8f88eb1fdf84c7d
Piet Klop <p.klop@viscon.eu>
2022-07-23 09:47:06 +0200

    UI family overview: Add display names

--- Messages/UI/Overview/FamilyViewModel.cs
+++ Messages/UI/Overview/FamilyViewModel.cs
@@ -1,12 +1,17 @@
-﻿using Messages.UI.Infrastructure;
+﻿using System.ComponentModel;
+using Messages.UI.Infrastructure;
 
 namespace Messages.UI.Overview
 {
     public class FamilyViewModel
     {
+        [DisplayName("Display name")]
+        [Description("The name will be printed like shown in this column")]
         public string DisplayName { get; set; }
+        [DisplayName("First name")]
         [ColumnCellsUnderline]
         public string FirstName { get; set; }
+        [DisplayName("Last name")]
         [ColumnCellsUnderline]
         public string LastName { get; set; }
         public string City { get; set; }
